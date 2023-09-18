2aae3b3878c45bcce954c5649acf32abbac44a67
palakgoel30 <gpalak7055@gmail.com>
2023-09-18 15:26:21 +0530

    Modified Bubble Sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,8 +1,12 @@
-n = [7,8,3,1,2]
-for i in range(len(n)-1):
-    for j in range(len(n)-i-1):
-        if(n[j] > n[j+1]):
-            temp = n[j]
-            n[j] = n[j+1]
-            n[j+1] = temp
-print(n)
+def bubble_sort(n):
+    for i in range(len(n) - 1):
+        for j in range(len(n) - i - 1):
+            if (n[j] > n[j + 1]):
+                temp = n[j]
+                n[j] = n[j + 1]
+                n[j + 1] = temp
+    return n
+
+
+n = list(input())
+print(bubble_sort(n))
