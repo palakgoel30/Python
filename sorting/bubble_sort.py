ef3508cf4cdb92711fb9da3cc5ecec82333afed9
palakgoel30 <gpalak7055@gmail.com>
2023-09-18 15:49:10 +0530

    Modified Bubble Sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,4 +1,4 @@
-#Time Complexity is O(n^2)
+# Time Complexity is O(n^2)
 # Bubble sort sorts in an ascending order,
 # loop run for n-1 times
 # It first puts a larger value at the last.
@@ -6,13 +6,15 @@ def bubble_sort(n):
     for i in range(len(n) - 1):
         for j in range(len(n) - i - 1):
             if (n[j] > n[j + 1]):
-                swapping(n,j)
+                swapping(n, j)
     return n
 
-def swapping(n,j):
+
+def swapping(n, j):
     temp = n[j]
     n[j] = n[j + 1]
     n[j + 1] = temp
+
 
 n = list(input())
 print(bubble_sort(n))
