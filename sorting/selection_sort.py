5cec7707972116a694b1a827abf1166c0759bbf7
palakgoel30 <gpalak7055@gmail.com>
2023-09-18 17:53:32 +0530

    Modified selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,12 +1,24 @@
-n = [7,8,3,1,2]
+# Time complexity is O(n^2)
+# selection sort sorts in an ascending order.
+# Loop run n-1 times
+# It puts smallest element at starting index.
 
-for i in range(len(n)-1):
-    smallest = i
-    for j in range(i+1,len(n)):
-        if(n[smallest] > n[j]):
-            smallest = j
+def selection_sort(n):
+    for i in range(len(n) - 1):
+        smallest = i
+        for j in range(i + 1, len(n)):
+            if (n[smallest] > n[j]):
+                smallest = j
+        swapping(n, i, smallest)
+
+    return n
+
+
+def swapping(n, i, smallest):
     temp = n[smallest]
     n[smallest] = n[i]
     n[i] = temp
-print(n)
 
+
+n = list(input())
+print(selection_sort(n))
