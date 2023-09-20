3006e262e42723e71781990faf5665d39c922d33
palakgoel30 <gpalak7055@gmail.com>
2023-09-20 17:56:29 +0530

    Modified insertion sort

--- sorting/insetion_sort.py
+++ sorting/insetion_sort.py
@@ -1,13 +1,15 @@
-n = [7,8,3,1,2]
-for i in range(1,len(n)):
-    current_ele = n[i]
-    j = i-1
-    while(j>=0 and current_ele < n[j]):
-        n[j+1] = n[j]
-        j = j -1
-    n[j+1] = current_ele
-print(n)
+def insertion_sort(n):
+    for i in range(1,len(n)):
+        current_ele = n[i]
+        j = i-1
+        while(j>=0 and current_ele < n[j]):
+            n[j+1] = n[j]
+            j = j -1
+        n[j+1] = current_ele
+    return n
 
+n = list(input())
+print(insertion_sort(n))
 
 
 
