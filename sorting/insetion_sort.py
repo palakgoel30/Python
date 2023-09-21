b41e1ae6b7f31aad140f92e4766829fcac63e2f6
palakgoel30 <gpalak7055@gmail.com>
2023-09-21 09:44:30 +0530

    Modified Insertion Sort

--- sorting/insetion_sort.py
+++ sorting/insetion_sort.py
@@ -1,10 +1,10 @@
 # Time complexity is O(n^2)
-# Divided into unsorted and sorted part.
+# Split a list into 2 parts unsorted and sorted array.
 def insertion_sort(n):
     for i in range(1,len(n)):
         current_ele = n[i]
         j = i-1
-        while(j>=0 and current_ele < n[j]):
+        while(j>=0 and current_ele < n[j]): # Compare the current element to next element
             n[j+1] = n[j]
             j -= 1
         n[j+1] = current_ele
